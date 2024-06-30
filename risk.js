aebfd3b7f42e32ead1069055ec1a9771936a6399
Vijayteja2604 <162493457+Vijayteja2604@users.noreply.github.com>
2024-06-30 18:09:20 +0530

    Update risk.js

--- risk.js
+++ risk.js
@@ -2,6 +2,7 @@
             const menu = document.getElementById('menu');
             menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
         }
+
         let signin=document.getElementById("signin");
         let signup=document.getElementById("signup");
         let name=document.getElementById("Namefield");
@@ -9,21 +10,33 @@
         let forgot=document.getElementById("Forgot");
 
     
-            signin.onclick = function() {
-                title.innerHTML = "Sign In";
-                name.style.display = "none";
-                forgot.style.display = "block";
-                title.style.marginTop = "10px";
-                signin.style.transition = "background-color 0.3s ease-in-out";
-            
-            }
-        
+        signin.onclick = function() {
+            title.innerHTML = "Sign In";
+            name.style.display = "none";
+            title.style.marginTop = "10px";
+            forgot.innerHTML = `
+                <div>Forgot Password? <a href='#'>Click me</a></div>
+                <div style='margin-top: 10px;'>Don't have an account? <a href='#' id='signupLink'>Sign Up</a></div>
+            `; 
+            signup.style.display="none";
+            signin.style.display="block";
+            document.getElementById('signupLink').addEventListener('click', function(event) {
+                event.preventDefault(); 
+                signup.onclick();
+            });
+        }
         signup.onclick = function() {
             title.innerHTML = "Sign Up";
             name.style.display = "flex";
-            forgot.style.display = "none";
+            forgot.innerHTML = "Already have an account? <a href='#' id='signinLink'>Sign In</a>";
             title.style.marginTop = "10px";
             name.style.marginbottom = "10px";
-            signup.style.transition = "background-color 0.3s ease-in-out";
-            name.style.transition = "margin-top 0.3s ease-in-out";
+            signin.style.display="none";
+            signup.style.display="block";
+            document.getElementById('signinLink').addEventListener('click', function(event) {
+                event.preventDefault(); 
+                signin.onclick();
+            });
+            
+            
         }
